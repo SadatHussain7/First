37fafb31c2ee093992b04abe7cd35516f59a97b5
Sadathussain Javidahmed <sadathussain.j@intimetec.com>
2019-02-18 10:30:04 +0530

    first version of string reverse script

---
+++ reverse-str.ps1
@@ -0,0 +1,13 @@
+﻿#script to reverse a string
+
+Write-Host "Enter a string"
+$str = Read-Host
+
+$chararr = $str.ToCharArray()
+
+[array]::reverse($chararr)
+
+$revstr = -join($chararr)
+
+Write-Host "Original string is: $str"
+Write-Host "Reversed string is: $revstr"
