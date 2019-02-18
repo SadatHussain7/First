b44c850c0eabc1145745a2f983f35ca11873ddf1
Sadathussain Javidahmed <sadathussain.j@intimetec.com>
2019-02-18 14:39:33 +0530

    No change

---
+++ reverse-str.ps1
@@ -0,0 +1,11 @@
+Write-Host "Enter the string"
+$str = read-host
+
+$chararr = $str.ToCharArray()
+
+[array]::reverse($chararr)
+$revstr = -join($chararr)
+
+Write-Host "Reversed string is: $revstr"
+Write-Host "Original string is: $chararr"
+
