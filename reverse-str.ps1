2b061f0d5beb5b4aa9fa59099e9b4d4d10d72da4
Sadathussain Javidahmed <sadathussain.j@intimetec.com>
2019-02-18 14:10:02 +0530

    last few lines

--- reverse-str.ps1
+++ reverse-str.ps1
@@ -1,10 +1,5 @@
-﻿#script to reverse a string
-
-Write-Host "Enter a string"
-$str = Read-Host
-
-$chararr = $str.ToCharArray()
-
+﻿
 [array]::reverse($chararr)
+$revstr = -join($chararr)
 
 Write-Host "Reversed string is: $revstr"
